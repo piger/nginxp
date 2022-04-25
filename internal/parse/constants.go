5e8fa23d76e09d4ee19ad0bf4c4c7b470ba9c6e6
Daniel Kertesz <daniel@spatof.org>
2022-04-25 21:51:45 +0100

    add names for constants

--- internal/parse/constants.go
+++ internal/parse/constants.go
@@ -9,6 +9,20 @@ const (
 	itemVariable   // variable starting with '$', such as '$hello'
 	itemLeftBlock  // left block delimiter
 	itemRightBlock // right block delimiter
+	itemDirective  // a configuration directive, such as "server", "location" or "proxy_set_header"
+	itemArgument   // an argument of a directive, which can be a quoted string or a raw string
+	itemTerminator // the character ';' which terminates a directive
+	itemWord
+	itemNewline
 )
 
 const eof = -1
+
+var itemName = map[itemType]string{
+	itemError:   "error",
+	itemEOF:     "EOF",
+	itemComment: "comment",
+	itemString:  "quoted string",
+	itemWord:    "word",
+	itemNewline: "newline",
+}
