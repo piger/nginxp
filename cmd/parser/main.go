b7f7b0fe55a4174d233f9a33e85b598be6307cc4
Daniel Kertesz <daniel@spatof.org>
2022-04-26 11:33:17 +0100

    support reading configuration dumps

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -1,21 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/piger/nginxp/internal/parse"
 )
 
-func readFile(filename string) (string, error) {
-	content, err := os.ReadFile(filename)
+var (
+	magicHeader    = regexp.MustCompile(`^# configuration file ([^:]+):\n$`)
+	defaultSection = "__DEFAULT__"
+)
+
+var usage = func() {
+	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s: <filename> [section]\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+// unpackConfigurationDump reads a configuration dump produced by "nginx -T" and extracts each file
+// separately; the returned map maps the name of the files in the configuration dump with their contents.
+// This function also supports reading a single configuration file, in which case its contents will be stored
+// in the special "__DEFAULT__" key.
+func unpackConfigurationDump(filename string) (map[string]string, error) {
+	fh, err := os.Open(filename)
 	if err != nil {
-		return "", err
+		return nil, err
+	}
+	defer fh.Close()
+
+	contentMap := make(map[string]string)
+	current := defaultSection
+	var lines []string
+
+	r := bufio.NewReader(fh)
+
+Loop:
+	for {
+		line, err := r.ReadString('\n')
+		switch {
+		case errors.Is(err, io.EOF):
+			break Loop
+		case err != nil:
+			return nil, err
+		}
+
+		m := magicHeader.FindStringSubmatch(line)
+		switch {
+		case m == nil:
+			lines = append(lines, line)
+		case len(m) == 1:
+			return nil, fmt.Errorf("invalid magic header line: %q", line)
+		case len(m) > 1:
+			if len(lines) > 0 {
+				contentMap[current] = strings.Join(lines, "")
+			}
+			lines = nil
+			current = m[1]
+		}
 	}
 
-	return string(content), nil
+	contentMap[current] = strings.Join(lines, "")
+
+	return contentMap, nil
 }
 
 func run() error {
@@ -24,16 +77,35 @@ func run() error {
 		return errors.New("missing filename parameter")
 	}
 
-	contents, err := readFile(filename)
+	var section string
+	if len(flag.Args()) > 1 {
+		section = flag.Arg(1)
+	}
+
+	filesMap, err := unpackConfigurationDump(filename)
 	if err != nil {
 		return err
 	}
 
-	parse.LexerPlayground(filename, contents)
+	if section != "" {
+		contents, ok := filesMap[section]
+		if !ok {
+			return fmt.Errorf("section %q not found", section)
+		}
+		parse.LexerPlayground(section, contents)
+	} else {
+		contents, ok := filesMap[defaultSection]
+		if !ok {
+			return errors.New("a configuration dump was read but no section was specified")
+		}
+		parse.LexerPlayground(defaultSection, contents)
+	}
+
 	return nil
 }
 
 func main() {
+	flag.Usage = usage
 	flag.Parse()
 
 	if err := run(); err != nil {
