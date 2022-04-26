9dab4236fc0082bddf9ccbc827f29d94eb206a48
Daniel Kertesz <daniel@spatof.org>
2022-04-26 19:19:18 +0100

    update playground

--- internal/parse/playground.go
+++ internal/parse/playground.go
@@ -1,6 +1,8 @@
 package parse
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // LexerPlayground is a "playground" function that showcase the lexer.
 func LexerPlayground(filename, contents string) {
@@ -11,5 +13,28 @@ func LexerPlayground(filename, contents string) {
 		} else {
 			fmt.Printf("%s (%s)", token.val, token.typ)
 		}
+	}
+
+	fmt.Printf("\n\nPARSER TREE TEST ===>\n")
+
+	t, err := Parse(filename, contents)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, node := range t.Root.Nodes {
+		switch n := node.(type) {
+		case *DirectiveNode:
+			fmt.Printf("%s ", n)
+			for _, arg := range n.Args {
+				fmt.Printf("%s ", arg)
+			}
+			fmt.Println()
+		case *EmptyLineNode:
+			fmt.Println()
+		case *CommentNode:
+			fmt.Println(n)
+		}
+
 	}
 }
