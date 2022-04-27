4478760a2ccb47afe8509857d11cd7963c8a8eeb
Daniel Kertesz <daniel@spatof.org>
2022-04-27 15:03:45 +0100

    add opentracing_trace_locations

--- internal/parse/bitmask_extra.go
+++ internal/parse/bitmask_extra.go
@@ -10,6 +10,7 @@ func init() {
 	dirMask["opentracing_propagate_context"] = []int{NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_NOARGS}
 	dirMask["opentracing_tag"] = []int{NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE2}
 	dirMask["opentracing_operation_name"] = []int{NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1}
+	dirMask["opentracing_trace_locations"] = []int{NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_FLAG}
 
 	// ngx_http_js_module
 	// http://nginx.org/en/docs/http/ngx_http_js_module.html
