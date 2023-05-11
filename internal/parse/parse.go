586567cb1c2fbb86c078960f15dd24f3c60cb758
Daniel Kertesz <daniel@spatof.org>
2023-05-11 11:54:25 +0100

    add copyright notes

--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -1,3 +1,9 @@
+// Some of this code is from https://go.dev/src/text/template/parse/
+
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package parse
 
 import (
